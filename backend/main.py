d13ab18abb91a7cd6c3838638c299e3666bd047a
Daiwik Palavarapu <Daiwikpalavarapu755@gmail.com>
2025-11-11 14:04:08 +0530

    small edits

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,9 @@
-
+from resume_parser.extract_text import resume
 
 print("Welcome to autoapply.ai!")
 
-resume=input("Enter your Resume file path:")
-keyword=input("enter any  one special keyword to search(like cyber or iot):")
-
+r=input("Enter your Resume file path:")
+resume(r)
+keyword=input("enter any  one special keyword to search(like cyber or iot (use ',' for separation )): ")
 print("Searching for jobs based on the resume and keywords...")
-
 print("Done!")
--- backend/resume_parser/extract_text.py
+++ backend/resume_parser/extract_text.py
@@ -1,12 +1,15 @@
 import pymupdf
-path=""#path to be inserted here
-d=pymupdf.open(path)
+# path=""#path to be inserted here
 
-full_text=""
-for page_num in range(d.page_count):
-    page=d.load_page(page_num)
-    text=page.get_text()
-    full_text+=text
-print(full_text)
 
-print(d.metadata)
+def resume(path):
+    d=pymupdf.open(path)
+    full_text=""
+    for page_num in range(d.page_count):
+       page=d.load_page(page_num)
+       text=page.get_text()
+       full_text+=text
+    print(full_text)
+    print(d.metadata)
+    
+
